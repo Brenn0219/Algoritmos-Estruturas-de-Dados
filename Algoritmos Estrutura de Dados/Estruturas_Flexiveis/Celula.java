3cc4eafdb31c20032bab8aa7014f57600e76388e
unknown <Brenno@988174506343.newtonpaiva.edu.br>
2023-02-05 23:14:25 -0200

    ListaEncadeada

---
+++ Algoritmos Estrutura de Dados/Estruturas_Flexiveis/Celula.java
@@ -0,0 +1,11 @@
+package Estruturas_Flexiveis;
+
+public class Celula {
+    public int cell;
+    public Celula next;
+
+    public Celula(int x) {
+        cell = x;
+        next = null;    
+    }
+}
