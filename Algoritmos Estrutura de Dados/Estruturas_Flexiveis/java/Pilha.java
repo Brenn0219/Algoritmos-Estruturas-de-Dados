8736e38e8fbeb57d5dd772d77764713a3ebc95d0
brenn0219 <brennoaugusto2109@gamil.com>
2023-02-09 22:13:11 -0200

    Pilha Encadeada

--- Algoritmos Estrutura de Dados/Estruturas_Flexiveis/java/Pilha.java
+++ Algoritmos Estrutura de Dados/Estruturas_Flexiveis/java/Pilha.java
@@ -7,9 +7,50 @@ class Pilha {
         top = null;
     }
 
-    // Metodo de Inserir (Empilhar)
+    // Metodo de Inserir (push)
     public void push(int x) {
-        top = new CelulaSimples(x);
+        CelulaSimples tmp = new CelulaSimples(x);
+        tmp.next = top;
+        top = tmp;
+        tmp = null;
+    }
+
+    // Metodo de Remover (pop)
+    public int pop() throws Exception {
+        if(top == null) {
+            throw new Exception("Erro ao Remover");
+        }
+
+        int numberRemoved = top.cell;
+        CelulaSimples tmp = top;
+
         top = top.next;
+        tmp.next = tmp = null;
+
+        return numberRemoved;
+    }
+
+    // Metodo de Mostrar os Elementos
+    public void show() {
+        for(CelulaSimples i = top; i != null; i = i.next) {
+            if(i.next != null) {
+                System.out.print(i.cell + " - ");
+            } else {
+                System.out.println(i.cell);
+            }
+        }
+    }
+
+    // Metodo de Mostrar os Elementos de traz para frente
+    private void showRec(CelulaSimples i) {
+        if(i != null) {
+            showRec(i.next);
+            System.out.print(i.cell + " - ");
+        }
+        System.out.println(i.cell);
+    }
+
+    public void showRec() {
+        showRec(top);
     }
 }
