9b68b26205c85c3933a7620f393b2628fbe42f5a
brenn0219 <brennoaugusto2109@gamil.com>
2023-02-10 07:39:07 -0200

    Estruturas Flexiveis Completa

--- Algoritmos Estrutura de Dados/Lista/Lista.java
+++ Algoritmos Estrutura de Dados/Lista/Lista.java
@@ -1,5 +1,116 @@
 package Lista;
 
 public class Lista {
+    private int[] list;
+    private int size, counter;
+
+    public Lista() {
+        this(10);
+    }
+
+    public Lista(int x) {
+        size = x;
+        counter = 0;
+        list = new int[size];
+    }
+
+    // Metodo de Inserir no Inicio (metodo nada eficiente)
+    public void insertStart(int x) throws Exception {
+        if(counter >= size){
+           throw new Exception("Erro ao inserir - Tamanho");
+        } 
+  
+        for(int i = counter; i > 0; i--){
+           list[i] = list[i-1];
+        }
+  
+        list[0] = x;
+        counter++;
+    }
     
+    // Metodo de Inserir no Final
+    public void insertEnd(int x) throws Exception {
+        if(counter >= size) {
+            throw new Exception("Erro ao Inserir - Tamanho");
+        }
+
+        list[counter++] = x;
+    }
+
+    // Metodo para Inserir em qualquer Posicao (metodo nada eficiente)
+    public void insert(int x, int position) throws Exception {
+        if(position < 0 || position >= size || position > counter) {
+            throw new Exception("Erro ao Inserir - Posicao");
+        } else if (position == 0) {
+            insertStart(x);
+        } else if (position == size - 1) {
+            insertEnd(x);
+        } else {
+            for(int i = counter; i > position; i = i--) {
+                list[i] = list[i-1];
+            }
+    
+            list[position] = x;
+            counter++;
+        }
+    }
+
+    // Metodo de Remover no Inicio
+    public int removeStart() {
+        int numberRemoved = list[0];
+
+        for(int i = 0; i <= counter; i = i++) {
+            list[i] = list[i+1];
+        }
+
+        counter--;
+
+        return numberRemoved;
+    }
+
+    // Metodo de Remover no Final
+    public int removeEnd() {
+        return list[counter--];
+    }
+
+    // Metodo para Remover em qualquer Posicao 
+    public int remove(int x, int position) throws Exception  {
+        int numberRemoved = -1;
+
+        if(position < 0 || position > size) {
+            throw new Exception("Erro ao Remover - Posicao Invalida"); // Verifica a posicao
+        } else if (position == 0) {
+            numberRemoved = removeStart();
+        } else if (position == size) {
+            numberRemoved = removeEnd();
+        } else {
+            numberRemoved = list[position];
+
+            for(int i = position; i <= counter; i++) {
+                list[i] = list[i+1];
+            }
+        }
+
+        return numberRemoved;
+    }
+
+    // Retorna o Tamanho da Lista
+    public int size() {
+        return size;
+    }
+
+    // Metodo de Mostrar os Elementos
+    public void show() {
+        if(counter == size) {
+            --counter; // caso a lista esteja cheia
+        }
+
+        for(int i = 0; i <= counter; i++) {
+            if ((i + 1) != counter) {
+                System.out.print(list[i] + " - ");
+            } else {
+                System.out.println(list[i]);
+            }
+        }
+    }
 }
--- Algoritmos Estrutura de Dados/Main.java
+++ Algoritmos Estrutura de Dados/Main.java
@@ -1,52 +1,57 @@
+import Lista.Lista;
 import Pilha.Pilha;
 import Fila.Fila;
 import Estruturas_Flexiveis.java.*;
 
 class Main {
     public static void main(String[] args) throws Exception {
-        int size = 20;
+        int size = 20, half = size;
+        // Lista list = new Lista(size);
         // Pilha stack = new Pilha(size);
         // Fila queue = new Fila(size);
         // ListaEncadeada linkedList = new ListaEncadeada();
         // ListaDuplamenteEncadadeada doublyLinkedList = new ListaDuplamenteEncadadeada();
-        // int linkedListNumber = size;
-        PilhaEncadeada chainedStack = new PilhaEncadeada();
-        FilaEncadeada chainedQueue = new FilaEncadeada();
+        // PilhaEncadeada chainedStack = new PilhaEncadeada();
+        // FilaEncadeada chainedQueue = new FilaEncadeada();
 
         System.out.println(" === Inserir ===");
 
         for(int i = 0; i < size; i++) { 
+            // list.insert(half--, i);
             // stack.push(numbeRandom.nextInt());
             // queue.enqueue(i);
-            // linkedList.insert(linkedListNumber--, i);
-            // doublyLinkedList.insert(linkedListNumber--, i);
-            chainedStack.push(i);
-            chainedQueue.enqueue(i);
+            // linkedList.insert(half--, i);
+            // doublyLinkedList.insert(half--, i);
+            // chainedStack.push(i);
+            // chainedQueue.enqueue(i);
         }
 
+        // list.show();
         // stack.show();
         // queue.show();
         // linkedList.show();
         // doublyLinkedList.showPrevious();
-        chainedStack.show();
-        chainedQueue.show();
+        // chainedStack.show();
+        // chainedQueue.show();
 
         System.out.println(" === Remover ===");
 
         for(int i = 0; i < (size / 2); i++) {
+            // list.removeEnd();
             // stack.pop();
             // queue.dequeue();
             // linkedList.remove(i);
             // doublyLinkedList.remove(i);
-            chainedStack.pop();
-            chainedQueue.dequeue();
+            // chainedStack.pop();
+            // chainedQueue.dequeue();
         }
 
+        // list.show();
         // stack.show();
         // queue.show();
         // linkedList.show();
         // doublyLinkedList.showPrevious();
-        chainedStack.showRec();
-        chainedQueue.show();
+        // chainedStack.showRec();
+        // chainedQueue.show();
     }
 }
